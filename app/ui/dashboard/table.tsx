1c4144e114c765adbbdc455f87c287b5d3805d87
nicoisz <nicolasignacio.sz@gmail.com>
2024-07-02 23:04:41 -0600

    fix on search

--- app/ui/dashboard/table.tsx
+++ app/ui/dashboard/table.tsx
@@ -1,9 +1,4 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchCardByWord, fetchFilteredInvoices } from '@/app/lib/data';
-import { CardTableType } from '@/app/lib/definitions';
+import { fetchCardByWord } from '@/app/lib/data';
 import CardTable from '../card/cards';
 
 
@@ -20,7 +15,7 @@ export default async function CardsTable({
     <div className="mt-6 mb-6 w-full">
       {cards &&
         <div className="flex flex-row flex-wrap justify-center gap-2 col-3">
-            { cards != false ?  <CardTable card={cards} title={title}></CardTable>
+            { cards != false ?  <CardTable card={cards} title={''}></CardTable>
 
             : <h1>There is no card with this terms</h1>}
       </div>
