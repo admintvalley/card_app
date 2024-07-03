2fbd958eeac9d7f8f82195776b741a64a071fd2f
nicoisz <nicolasignacio.sz@gmail.com>
2024-07-02 22:42:46 -0600

    change on side menu

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,14 +14,14 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Profile', href: '/dashboard/profile', icon: UserCircleIcon },
+  // { name: 'Profile', href: '/dashboard/profile', icon: UserCircleIcon },
 
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  // {
+  //   name: 'Invoices',
+  //   href: '/dashboard/invoices',
+  //   icon: DocumentDuplicateIcon,
+  // },
+  // { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
